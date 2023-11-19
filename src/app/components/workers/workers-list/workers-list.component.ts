1ae4353ee2e822193307f2456964d220fcb28f5c
marcoantonio40 <marcoudi40@gmail.com>
2023-11-19 07:24:34 -0300

    Added new component workers

---
+++ src/app/components/workers/workers-list/workers-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workers-list',
+  templateUrl: './workers-list.component.html',
+  styleUrls: ['./workers-list.component.css']
+})
+export class WorkersListComponent {
+
+}
