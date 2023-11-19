f503f5c144a735d2de1ba8bb923b13a7a4435fe7
marcoantonio40 <marcoudi40@gmail.com>
2023-11-19 08:28:46 -0300

    Added tables

--- src/app/components/workers/workers-list/workers-list.component.ts
+++ src/app/components/workers/workers-list/workers-list.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Workers } from 'src/app/models/workers';
 
 @Component({
   selector: 'app-workers-list',
   templateUrl: './workers-list.component.html',
   styleUrls: ['./workers-list.component.css']
 })
-export class WorkersListComponent {
+export class WorkersListComponent implements OnInit{
+
+  ELEMENT_DATA: Workers[] = [
+    {
+      id: "1",
+      name: "Marco Antonio",
+      cpf: "055.818.520-66",
+      phone: "(34)995565658",
+      start_date: '15/08/2022',
+      usertype: ['0']
+
+    }
+  ]
+  displayedColumns: string[] = ['id', 'name', 'cpf', 'phone', 'start_date', 'acoes'];
+  dataSource = new MatTableDataSource<Workers>(this.ELEMENT_DATA);
+
+  
+  
+
+constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 
 }
+
+
---
+++ src/app/models/workers.ts
@@ -0,0 +1,9 @@
+export interface Workers {
+    id?: string;
+    name: string;
+    cpf: string;
+    phone: string;
+    start_date: any;
+    usertype: string[];
+
+}
