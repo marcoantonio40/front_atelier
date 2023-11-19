d5f1a1643bd687a47404c65e16c1cb295d2546e5
marcoantonio40 <marcoudi40@gmail.com>
2023-11-19 07:29:17 -0300

    Added router workers list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { WorkersListComponent } from './components/workers/workers-list/workers-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      {path: 'home', component: HomeComponent}
+      {path: 'home', component: HomeComponent},
+      {path: 'workers', component: WorkersListComponent}
     ]
   }
 ];
