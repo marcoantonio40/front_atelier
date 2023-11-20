3ecc3b7a22e86784d8ff3bec0335c44cc789ef00
marcoantonio40 <marcoudi40@gmail.com>
2023-11-20 10:29:53 -0300

    Validate form fields

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credentials } from 'src/app/models/credentials';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+creds: Credentials = {
+  email:'',
+  password: ''
+}
+
+email = new FormControl(null, Validators.email);
+password = new FormControl(null, Validators.minLength(8));
+
+constructor(){}
+  
+ngOnInit(): void {
+    
+}
+
+validFields(): boolean{
+  if(this.email.valid && this.password.valid){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 }
