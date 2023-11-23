494fef42a80fd2c73ddf0bf99515869da7d98223
marcoantonio40 <marcoudi40@gmail.com>
2023-11-23 07:50:50 -0300

    New router to component workers create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { HomeComponent } from './components/home/home.component';
 import { WorkersListComponent } from './components/workers/workers-list/workers-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './auth/auth.guard';
+import { WorkersCreateComponent } from './components/workers/workers-create/workers-create.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent,canActivate: [AuthGuard], children: [
       {path: 'home', component: HomeComponent},
-      {path: 'workers', component: WorkersListComponent}
+      {path: 'workers', component: WorkersListComponent},
+      {path: 'workers/create', component: WorkersCreateComponent}
     ]
   }
 ];
