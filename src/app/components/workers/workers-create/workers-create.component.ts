6e114b06b0e20e0ac5555e743989218bd2d02fe0
marcoantonio40 <marcoudi40@gmail.com>
2023-11-23 20:54:59 -0300

    Added config in form create worker

--- src/app/components/workers/workers-create/workers-create.component.ts
+++ src/app/components/workers/workers-create/workers-create.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-workers-create',
   templateUrl: './workers-create.component.html',
   styleUrls: ['./workers-create.component.css']
 })
-export class WorkersCreateComponent {
+export class WorkersCreateComponent implements OnInit{
+  
+  name: FormControl = new FormControl(null, [Validators.minLength(10), Validators.maxLength(50)]);
+  cpf: FormControl = new FormControl(null, [Validators.minLength(11), Validators.maxLength(11), Validators.required]);
+  phone: FormControl = new FormControl(null, [Validators.minLength(11), Validators.maxLength(11)]);
+  email: FormControl = new FormControl(null, Validators.email);
 
+  constructor(){}
+  
+  ngOnInit(): void {
+  }
+
+  validFields(): boolean {
+    return this.name.valid && 
+    this.cpf.valid && 
+    this.email.valid && 
+    this.phone.valid;
+  }
 }
