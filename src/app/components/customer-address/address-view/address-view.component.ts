0ca550c703946fb6e2e20ef1dceed0b8f766b49c
marcoudi40 <marcoudi40@gmail.com>
2024-01-31 20:08:56 -0300

    New changes

--- src/app/components/customer-address/address-view/address-view.component.ts
+++ src/app/components/customer-address/address-view/address-view.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { CustomersAddress } from 'src/app/models/customerAddress';
 
 @Component({
   selector: 'app-address-view',
   templateUrl: './address-view.component.html',
   styleUrls: ['./address-view.component.css']
 })
-export class AddressViewComponent {
+export class AddressViewComponent implements OnInit{
 
+  
+
+  ELEMENT_DATA: CustomersAddress[] = [
+    {
+      id: '12345',
+      street: 'Marco Antonio',
+      number: '12345',
+      neighborhood: 'Centro',
+      cep:'37402045',
+      complement: 'Próximo a igreja'
+
+    }
+]
+  displayedColumns: string[] = ['street', 'number','neighborhood', 'cep', 'complement'];
+  dataSource = new MatTableDataSource<CustomersAddress>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngOnInit(): void { }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
